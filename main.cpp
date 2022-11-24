8052e7c0693f6d615accbe55a1cd1b52ba4d76bc
mattianatale <117930083+mattianatale@users.noreply.github.com>
2022-11-24 08:27:53 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 using namespace std;
-
-int main() {
+int main(){
+    int a=1;
+    int b=1;
+    int c;
+    int n;
+    cin>>n;
+    if(n>=2){
+        for (int i=0; i<n; i++){
+        cout<<a<<endl;
+        c=a;
+        a=b;
+        b=a+c;
+        }
+    }
+    else{
+    cout<<"errore";
+    }
    return 0;
 }
